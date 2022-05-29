4cdf3a12e378dec56c9221fb29d9570cdf549335
dashakozachenko <dkozacenko4@gmail.com>
2022-05-29 14:00:01 +0200

    js

--- js/solution.js
+++ js/solution.js
@@ -1,51 +1,53 @@
 'use strict';
 
-const itemLi = document.querySelectorAll('li');
-console.log(itemLi);
+(function () {
+    const itemLi = document.querySelectorAll('li');
+    console.log(itemLi);
 
-const ul = document.body.firstElementChild;
+    const ul = document.body.firstElementChild;
 
-const ulAttributes = ul.attributes;
-console.log(ulAttributes);
+    const ulAttributes = ul.attributes;
+    console.log(ulAttributes);
 
-const ulObj = {
-    attributesItem : [],
-    attributesValue : [],
-
-    getUl (attributes) {
-        for (let attr of attributes ) {
-            this.attributesItem.push(attr);
-            this.attributesValue.push(attr.textContent);
+    const ulObj = {
+        attributesItem : [],
+        attributesValue : [],
+        getUl (attributes) {
+            for (let attr of attributes ) {
+                this.attributesItem.push(attr);
+                this.attributesValue.push(attr.textContent);
+            }
+            console.log(this.attributesItem);
+            console.log(this.attributesValue);
         }
-        console.log(this.attributesItem);
-        console.log(this.attributesValue);
+    };
+
+    ulObj.getUl(ulAttributes);
+
+
+    const newName = 'Jon';
+
+    function enterLastLi (item , name) {
+        item.lastElementChild.innerHTML = `My name is ${name}`;
+        console.log(item.lastElementChild.textContent);
     }
-};
 
-ulObj.getUl(ulAttributes);
+    enterLastLi(ul , newName);
 
+    function getAttr (list ,attrName, attrValue){
+        list.setAttribute(attrName, attrValue);
+        console.log(list);
+        console.log(list.getAttribute('data-my-name'));
+    }
 
-const newName = 'Jon';
+    getAttr(ul, 'data-my-name', newName);
 
-function enterLastLi (item , name) {
-    item.lastElementChild.innerHTML = `My name is ${name}`;
-    console.log(item.lastElementChild.textContent);
-}
+    function deleteAttr (list , attrName){
+        list.removeAttribute(attrName);
+        console.log(list);
+        console.log(list.getAttribute('date-dog-tail'));
+    }
 
-enterLastLi(ul , newName);
+    deleteAttr(ul, 'data-dog-tail');
 
-function getAttr (list ,attrName, attrValue){
-    list.setAttribute(attrName, attrValue);
-    console.log(list);
-    console.log(list.getAttribute('data-my-name'));
-}
-
-getAttr(ul, 'data-my-name', newName);
-
-function deleteAttr (list , attrName){
-    list.removeAttribute(attrName);
-    console.log(list);
-    console.log(list.getAttribute('date-dog-tail'));
-}
-
-deleteAttr(ul, 'data-dog-tail');
+})();
